c0c0342bc2dcc3ad5af7746f78122b9e4b3ff980
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-04-05 20:18:30 -0300

    test: teste de emprestimo entity para sem atraso

---
+++ src/enterprise/entities/emprestimo.entity.spec.js
@@ -0,0 +1,12 @@
+const emprestimoEntity = require('./emprestimo.entity');
+
+describe('Emprestimos Entity', function () {
+  test('Calcular multa sem atraso', function () {
+    const resultado = emprestimoEntity.calcularMulta({
+      data_retorno: '2025-04-05',
+      data_devolucao: '2025-04-04',
+    });
+
+    expect(resultado).toBe('Multa por atraso: R$ 0');
+  });
+});
