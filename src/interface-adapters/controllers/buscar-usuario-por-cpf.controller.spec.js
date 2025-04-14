fce3ba533406ee3f1c6c7d9c6f7c302c22f66549
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-04-13 21:52:33 -0300

    test: cria teste que retorna null caso nao encontre usuario com o cpf informado

--- src/interface-adapters/controllers/buscar-usuario-por-cpf.controller.spec.js
+++ src/interface-adapters/controllers/buscar-usuario-por-cpf.controller.spec.js
@@ -31,4 +31,23 @@ describe('Buscar usuario por cpf Controller', function () {
     expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledWith(httpRequest.params);
     expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledTimes(1);
   });
+
+  test('Deve retornar um httpResponse 200 e null se não for encotrado nenhum usuario com o CPF', async function () {
+    buscarUsuarioPorCPFUseCase.mockResolvedValue(Either.Right(null));
+
+    const httpRequest = {
+      params: {
+        CPF: '123.123.123-12',
+      },
+    };
+
+    const response = await buscarUsuarioPorCpfController({
+      buscarUsuarioPorCPFUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(200, null));
+    expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledWith(httpRequest.params);
+    expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledTimes(1);
+  });
 });
