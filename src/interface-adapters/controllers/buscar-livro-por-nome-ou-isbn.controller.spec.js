ea22d898f2e21ecba195f60effc193c05a786eac
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-04-21 21:06:29 -0300

    test: inclui demais testes para controller de busca de livro por nome ou isbn

--- src/interface-adapters/controllers/buscar-livro-por-nome-ou-isbn.controller.spec.js
+++ src/interface-adapters/controllers/buscar-livro-por-nome-ou-isbn.controller.spec.js
@@ -1,4 +1,5 @@
-const { Either } = require('../../shared/errors');
+const { ZodError } = require('zod');
+const { Either, AppError } = require('../../shared/errors');
 const httpResponse = require('../../shared/helpers/http.response');
 const buscarLivroOorNomeOuIsbnController = require('./buscar-livro-por-nome-ou-isbn.controller');
 
@@ -29,5 +30,44 @@ describe('Buscar livro por nome ou ISBN Controller', function () {
       httpRequest.query,
     );
     expect(buscarLivroPorNomeOuISBNUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve retornar um httpResponse 200 e um array vazio se não forem encontrados com o valor informado', async function () {
+    buscarLivroPorNomeOuISBNUseCase.mockResolvedValue(Either.Right([]));
+    const httpRequest = {
+      query: {
+        valor: 'nome_valido',
+      },
+    };
+
+    const response = await buscarLivroOorNomeOuIsbnController({
+      buscarLivroPorNomeOuISBNUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(200, []));
+    expect(buscarLivroPorNomeOuISBNUseCase).toHaveBeenLastCalledWith(
+      httpRequest.query,
+    );
+    expect(buscarLivroPorNomeOuISBNUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve retornar um throw AppError se o buscarLivroPorNomeOuISBNUseCase ee httpRequest não forem fornecidos', function () {
+    expect(() => buscarLivroOorNomeOuIsbnController({})).rejects.toThrow(
+      new AppError(AppError.dependencias),
+    );
+  });
+
+  test('Deve retornar um erro do zodValidator se der erro na validação', function () {
+    const httpRequest = {
+      query: {},
+    };
+
+    expect(() =>
+      buscarLivroOorNomeOuIsbnController({
+        buscarLivroPorNomeOuISBNUseCase,
+        httpRequest,
+      }),
+    ).rejects.toBeInstanceOf(ZodError);
   });
 });
