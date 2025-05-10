e344c0e24cffc215153ef6c595195cad1a089774
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-05-10 20:48:11 -0300

    test: cria teste para criação de livro sem campos

--- src/main/api/routes/livros.routes.test.js
+++ src/main/api/routes/livros.routes.test.js
@@ -20,4 +20,18 @@ describe('Livros Route', function () {
     expect(statusCode).toBe(201);
     expect(body).toBeNull();
   });
+
+  test('Deve retornar um erro com os campos obrigatórios ausentes', async function () {
+    const { statusCode, body } = await request(app).post('/livros').send({});
+
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('Erro na validação');
+    expect(body.erros.fieldErrors).toEqual({
+      nome: ['Nome é obrigatório'],
+      quantidade: ['Quantidade é obrigatório'],
+      autor: ['Autor é obrigatório'],
+      genero: ['Gênero é obrigatório'],
+      ISBN: ['ISBN é obrigatório'],
+    });
+  });
 });
